070dc2ec2b6a7f5a167ef5c16f2b072fdb421d41
Alex <alexex.by@gmail.com>
2018-09-20 22:55:42 +0300

    Implement From your school teacher

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 module.exports = function solveEquation(equation) {
   // your implementation
+  
+  // ax2 + bx + c
+  let a, b, c, d,
+      x1, x2, 
+      result,
+      quadraticEquation,
+      quadraticEquationArr;
+  
+  quadraticEquation = equation.replace(/(\s\*\s)+/g,'').replace(/(-\s)/g, '-').replace(/(\+\s)/g, '+');
+  quadraticEquationArr = quadraticEquation.split(' ');
+  
+  quadraticEquationArr.forEach(item => {
+    if (item.search(/\x\^2/ig) !== -1) {
+      a = item.replace(/x\^2/i, '');
+    } else if(item.search(/x/ig) !== -1) {
+      b = item.replace(/x/i, '');
+    } else {
+      c = item;
+    }
+  })
+  
+  d = Math.sqrt(b*b - 4*a*c);
+
+  x1 = Math.round((-b - d)/(2*a));
+  x2 = Math.round((-b + d)/(2*a));
+
+  if (x1 < x2) {
+    result = [x1, x2]
+  } else {
+    result = [x2, x1];
+  }
+  
+  return result;
 }
